8c77f840a1d9e26def41e0b04cdac3d47a3c0df8
mycomputerak47 <mycomputerak47@gmail.com>
2016-02-24 15:20:30 +0530

    Update downloadfile.php

--- downloadfile.php
+++ downloadfile.php
@@ -12,7 +12,11 @@ $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 $randfilename = substr(str_shuffle($chars), 0, 15);
 
 $storeLink = mysqli_query($conn, "INSERT INTO downloadlink VALUES('','$realFile','$randfilename','')");
-$downloadScript = "download.php?file=$randfilename";
+######################################################
+## add next two commented lines if you don't want a request using ajax
+## that way original file name is send to downloadfile.php?f=file_name using get method
+######################################################
+//$downloadScript = "download.php?file=$randfilename";
 //header('location:'.$downloadScript);
 echo $randfilename;
 }
