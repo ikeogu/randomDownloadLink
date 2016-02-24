c5b369938b6d28c1c72ee1c8e115064488353ea8
mycomputerak47 <mycomputerak47@gmail.com>
2016-02-24 15:18:29 +0530

    Added ajax request to download file

--- downloadfile.php
+++ downloadfile.php
@@ -1,6 +1,7 @@
 <?php
 require('connect.php');
 if(isset($_GET['f'])){
+//echo "hello";
 /*
 #
 #	ADD SOME SECURITY PARAMETERS FOR $_GET['f']
@@ -12,7 +13,8 @@ $randfilename = substr(str_shuffle($chars), 0, 15);
 
 $storeLink = mysqli_query($conn, "INSERT INTO downloadlink VALUES('','$realFile','$randfilename','')");
 $downloadScript = "download.php?file=$randfilename";
-header('location:'.$downloadScript);
+//header('location:'.$downloadScript);
+echo $randfilename;
 }
 else{
 echo 'your ip has been recorded';//give a redirect to another page
